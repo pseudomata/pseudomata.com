3527ad2f3c2c332cf35d1f5ba7339fb38bbab07d
Pseudomata <pseudomata@proton.me>
2023-12-18 17:29:06 -0500

    Simple test for if the directories exist.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import qualified Website (generate)
+import System.Directory (doesDirectoryExist)
 
 -- Paths to directories, assuming the command is run from the root of the repository.
 contentPages = "content/"
@@ -8,7 +9,18 @@ staticAssets = "static/"
 templatesDir = "templates/"
 outputDir = "generated/"
 
+checkDirectories :: [String] -> IO ()
+checkDirectories dirs = mapM_ checkDirectoryExistence dirs
+
+checkDirectoryExistence :: String -> IO ()
+checkDirectoryExistence dir = do
+  exists <- doesDirectoryExist dir
+  unless exists $ error $ "Error: Directory not found - " ++ dir
+
 main :: IO ()
 main = do
+  putStrLn "Preliminary checks..."
+  checkDirectories [ contentPages staticAssets templatesDir outputDir ]
+
   putStrLn "Generating static webpages..."
   Website.generate contentPages staticAssets templatesDir outputDir
