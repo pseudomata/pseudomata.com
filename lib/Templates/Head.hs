337d7e1a2f4c55b70394e421f830d469b35a1d43
Pseudomata <pseudomata@proton.me>
2023-12-19 13:02:14 -0500

    Operate on an array of fonts to generate the preloads.

--- lib/Templates/Head.hs
+++ lib/Templates/Head.hs
@@ -3,14 +3,22 @@
 import Text.Blaze.Html5 as H
 import Text.Blaze.Html5.Attributes as A
 
+fonts :: [String]
+fonts =
+  [ "/fonts/Hubot-Sans.woff2"
+  , "/fonts/Mona-Sans.woff2"
+  , "/fonts/Monaspace-Neon.woff2"
+  ]
+
+generateFontPreloads :: [String] -> Html
+generateFontPreloads = mapM_ (\fontPath -> link ! A.rel "preload" ! A.href (toValue fontPath) ! A.as "font" ! A.type_ "font/woff2" ! A.crossorigin "anonymous") fonts
+
 websiteHead :: Html
 websiteHead = H.head $ do
   meta ! A.charset "UTF-8"
   meta ! A.name "viewport" ! A.content "width=device-width, initial-scale=1.0"
 
-  link ! A.rel "preload" ! A.href "/fonts/Hubot-Sans.woff2" ! A.as "font" ! A.type_ "font/woff2" ! A.crossorigin "anonymous"
-  link ! A.rel "preload" ! A.href "/fonts/Mona-Sans.woff2" ! A.as "font" ! A.type_ "font/woff2" ! A.crossorigin "anonymous"
-  link ! A.rel "preload" ! A.href "/fonts/Monaspace-Neon.woff2" ! A.as "font" ! A.type_ "font/woff2" ! A.crossorigin "anonymous"
+  generateFontPreloads fonts
 
   link ! A.rel "preload" ! A.as "image" ! A.href "/images/pseudomata.png"
   link ! A.rel "icon" ! A.type_ "image/png" ! A.href "/images/pseudomata.png"
