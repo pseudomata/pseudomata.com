df608d9f01a1f71ac8ac8616c4d1abeb15b296fc
Pseudomata <pseudomata@proton.me>
2024-01-02 16:43:25 -0500

    Update year.

--- lib/Templates/Footer.hs
+++ lib/Templates/Footer.hs
@@ -6,7 +6,7 @@ import Text.Blaze.Html5.Attributes as A
 websiteFooter :: Html
 websiteFooter =
   footer $ do
-    "© 2023 "
+    "© 2024 "
     a ! A.href "/" $ b "Pseudomata"
     " · Website " a ! A.href "/privacy" $ b "Privacy Policy"
     " · This work is licensed under " a ! A.href "https://creativecommons.org/licenses/by-nc-nd/4.0/" $ b "CC BY-NC-ND 4.0"
